5468830aee60b46baf4d683961530dcba18fb4ab
Stephan Ude <ude@mediatis.de>
2016-12-01 18:37:40 +0100

    initial version (based on formrelay_salesforce)

---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "formrelay_pardot"
+#
+# Auto generated 13-05-2009 13:55
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Pardot Plugin',
+    'description' => 'Send form data to Pardot via FormHandlers',
+    'category' => 'be',
+    'author' => '',
+    'author_email' => '',
+    'shy' => '',
+    'dependencies' => 'cms',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'author_company' => '',
+    'version' => '0.0.1',
+    'constraints' => array(
+        'depends' => array(
+            'cms' => '',
+            'formrelay' => '0.0.1',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    'suggests' => array(
+    ),
+);

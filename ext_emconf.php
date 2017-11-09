13ae287ba5dc4ec84bb1889ff61213a28c23e72d
Michael Vöhringer <voehringer@mediatis.de>
2017-11-09 15:24:29 +0100

    init from formrelaiy_pardot

---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "formrelay_mail"
+#
+# Auto generated 13-05-2009 13:55
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Mail Plugin',
+    'description' => 'Send form data via Mail via FormHandlers',
+    'category' => 'be',
+    'author' => '',
+    'author_email' => '',
+    'shy' => '',
+    'dependencies' => 'cms',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'author_company' => '',
+    'version' => '0.0.1',
+    'constraints' => array(
+        'depends' => array(
+            'cms' => '',
+            'formrelay' => '0.0.9',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    'suggests' => array(
+    ),
+);
---
+++ ext_localconf.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+// register Hook to process data
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['formrelay']['dataProcessor'][] = 'Mediatis\\FormrelayPardot\\Hooks\\Mail';

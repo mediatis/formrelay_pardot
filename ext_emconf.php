20fcf8e81bca100506cac19b85225558c46f6cd7
Meelis Karulin <karulin@mediatis.de>
2018-12-14 18:18:59 +0100

    Remove comment block

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext: "formrelay_pardot"
-#
-# Auto generated 13-05-2009 13:55
-#
-# Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'Pardot Plugin',
     'description' => 'Send form data to Pardot via FormHandlers',

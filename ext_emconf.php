1c18792bb48c2cedcd7e2a22f07d2784107f24d5
Stephan Ude <ude@mediatis.de>
2019-09-03 16:09:11 +0200

    [TASK] cleanup, update composer.json (MEDO-241)

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext: "formrelay_mail"
-#
-# Auto generated 13-05-2009 13:55
-#
-# Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Mail Plugin',
     'description' => 'Send form data via Mail via FormHandlers',

81385165cbe8b7b863c94bf0d8f7e78cd525585c
Stephan Ude <ude@mediatis.de>
2021-03-03 12:23:20 +0100

    LMSCB-16: update to external PHP package form-relay/pardot

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,22 @@ if (!defined('TYPO3_MODE')) {
 }
 
 (function() {
-    $registry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Mediatis\Formrelay\Service\Registry::class);
-    $registry->registerDestination(\Mediatis\FormrelayPardot\Destination\Pardot::class);
+    /** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $dispatcher */
+    $dispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+
+    // relay initalization
+    $dispatcher->connect(
+        \FormRelay\Core\Service\RegistryInterface::class,
+        \Mediatis\Formrelay\Factory\RegistryFactory::SIGNAL_UPDATE_REGISTRY,
+        \Mediatis\FormrelayPardot\Initialization::class,
+        'initialize'
+    );
+
+    // configuration updater
+    $dispatcher->connect(
+        \Mediatis\Formrelay\Configuration\RouteConfigurationUpdaterInterface::class,
+        \Mediatis\Formrelay\Configuration\RouteConfigurationUpdaterInterface::SIGNAL_UPDATE_ROUTE_CONFIGURATION,
+        \Mediatis\FormrelayPardot\Configuration\ConfigurationUpdater::class,
+        'updateRouteConfiguration'
+    );
 })();

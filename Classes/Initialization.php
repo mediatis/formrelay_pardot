3e98dd7a70de03d4e2cd85f5208dce4a81f20051
Stephan Ude <ude@mediatis.de>
2021-03-11 19:12:21 +0100

    LMSCB-16: async form submissions updates

--- Classes/Initialization.php
+++ Classes/Initialization.php
@@ -4,11 +4,13 @@ namespace Mediatis\FormrelayPardot;
 
 use FormRelay\Core\Service\RegistryInterface;
 use FormRelay\Pardot\PardotInitialization;
+use FormRelay\Request\Route\RequestRoute;
 
 class Initialization
 {
     public function initialize(RegistryInterface $registry)
     {
         PardotInitialization::initialize($registry);
+        $registry->deleteRoute(RequestRoute::class);
     }
 }

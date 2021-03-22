a18428882d8a0ce6bf470ae3b63b77795d0be5b9
Stephan Ude <ude@mediatis.de>
2021-03-22 11:08:39 +0100

    LMSCB-16: using form-relay registration utilitiy

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,22 +4,12 @@ if (!defined('TYPO3_MODE')) {
 }
 
 (function() {
+    // relay initalization
+    \Mediatis\Formrelay\Utility\RegistrationUtility::registerInitialization(\Mediatis\FormrelayPardot\Initialization::class);
+
     /** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $dispatcher */
     $dispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
 
-    // relay initalization
-    $dispatcher->connect(
-        \FormRelay\Core\Service\RegistryInterface::class,
-        \Mediatis\Formrelay\Factory\RegistryFactory::SIGNAL_UPDATE_REGISTRY,
-        \Mediatis\FormrelayPardot\Initialization::class,
-        'initialize'
-    );
-
     // configuration updater
-    $dispatcher->connect(
-        \Mediatis\Formrelay\Configuration\RouteConfigurationUpdaterInterface::class,
-        \Mediatis\Formrelay\Configuration\RouteConfigurationUpdaterInterface::SIGNAL_UPDATE_ROUTE_CONFIGURATION,
-        \Mediatis\FormrelayPardot\Configuration\ConfigurationUpdater::class,
-        'updateRouteConfiguration'
-    );
+    \Mediatis\Formrelay\Utility\RegistrationUtility::registerRouteConfigurationUpdater(\Mediatis\FormrelayPardot\Configuration\ConfigurationUpdater::class);
 })();
